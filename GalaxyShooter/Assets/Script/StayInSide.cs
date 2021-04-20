70e7f5dd935549fb820c29e92fadd31829054902
nguyentranthyan <thyannguyentran@gmail.com>
2021-04-20 22:32:06 +0700

    Fix bug
    Game object not to go off the screen Using Mathf Clamp Function

---
+++ GalaxyShooter/Assets/Script/StayInSide.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInSide : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, -5f, 5f),
+			Mathf.Clamp(transform.position.y, -9f, 9f), transform.position.z);
+	}
+}
